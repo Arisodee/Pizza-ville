15b6d44d6065823aa150435b98b583c9223384eb
Arisodee <arisodee@gmail.com>
2020-10-19 23:59:23 +0300

    Get js to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,43 +1,35 @@
-$(document).ready(function() {
-    $("#order-details").hide();
-    });
-    
-    var priceTotal = [];
-    function orderDetails(size, crust, toppings, number) {
-      this.size = size;
-      this.crust = crust;
-      this.toppings = toppings;
-      this.number = number;
-      this.pizzaPrice = 0
-    }
-    orderDetails.prototype.pizzaCost = function() {
-      if (this.size === "small") {
-        return this.pizzaPrice += 500;
-      } else if (this.size === "medium") {
-        return this.pizzaPrice += 800;
-      } else if (this.size === "large") {
-        return this.pizzaPrice += 1200;
-      }
+ 
+  function size() {
+    let pizzaSize = document.getElementById("size").value;
+    return parseInt(pizzaSize);
+  }
+  function crust() {
+    let pizzaCrust = document.getElementById("crust").value;
+    return parseInt(pizzaCrust);
+  }
+  function toppings() {
+    let pizzaTopping = document.getElementById("toppings").value;
+    return parseInt(pizzaTopping);
+  }
+  function number() {
+    var pizzaNumber = document.getElementById("number").value;
+    return parseInt(pizzaNumber);
+  }
+  
+  function pizza(size, crust, toppings, number) {
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.number = number;
+  }
 
-      if (this.crust === "crispy") {
-        return this.pizzaPrice += 100;
-      } else if (this.crust === "stuffed") {
-        return this.pizzaPrice += 150;
-      } else if (this.crust === "nogluten") {
-        return this.pizzaPrice += 200;
-      } 
+  var inputtedValue = new pizza(size(), crust(), topping(), number());
 
-      if (this.toppings === "mushroom") {
-        this.pizzaPrice += 100;
-      } else if (this.toppings === "cheese") {
-        this.pizzaPrice += 150;
-      } else if (this.toppings === "olives") {
-        this.pizzaPrice += 100;
-      } else if (this.toppings === "bacon") {
-        this.pizzaPrice += 150;
-      } else if (this.toppings === "peperoni") {
-        this.pizzaPrice += 150;
-      } else if (this.toppings === "pineapple") {
-        this.pizzaPrice += 100;
-      }
-    };
+  var amountToPay =
+    (inputtedValue.Size + inputtedValue.crust + inputtedValue.topping) * inputtedValue.number;
+
+  function calculateCost(){
+  alert("You have Ordered " + number("")  +  " pizza."  +  ""  +  " Your total cost is kshs "  +  (amountToPay);
+  prompt("enter your location for delivery at Ksh 200")
+  alert("Expect your delivery within 30 minutes" + " Thank you for ordering from us")
+}
