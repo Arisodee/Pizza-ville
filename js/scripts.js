60363e0d76cf3f4c7c26e67224449dcbb088d51e
Arisodee <arisodee@gmail.com>
2020-10-20 13:47:21 +0300

    Correct indentation and and add a comment

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,13 @@
-// function pizza(size, crust, toppings , number) {
-//   this.size = size;
-//   this.crust = crust;
-//   this.toppings = toppings;
-//   this.number = number;
-  
-  
-  function size() {
+    function size() {
     let pizzaSize = $("#size").val();
     console.log ("pizzaSize", pizzaSize)
     return parseInt(pizzaSize);
-  }
+    }
   function crust() {
     let pizzaCrust = $("#crust").val();
     console.log ("pizzaCrust", pizzaCrust)
     return parseInt(pizzaCrust);
-  }
+    }
   function toppings() {
     let toppings = 0
     if ($("#mushroom").is(":checked")) {
@@ -37,24 +30,26 @@
     }
     console.log ("toppings", toppings)
     return toppings;
-  }
+    }
   function number() {
     let pizzaNumber = $("#number").val();
     console.log ("pizzaNumber", pizzaNumber)
     return parseInt(pizzaNumber);
-  }
+    }
   function amountToPay (){
     let totalAmount = (size() + crust() + toppings()) * number();
     console.log ("totalAmount", totalAmount)
     return parseInt(totalAmount);
-  }
+    }
 
 
   function calculateCost(){
   alert("You have Ordered " + number()  +  " pizza(s)."  +  ""  +  " Your total cost is kshs "  +  amountToPay());
   prompt("If you would like to have your pizza delivered at a cost of Ksh 100, please enter your location")
   alert("Your pizza will be ready in 30 minutes. " + " Thank you for ordering from us");
+  
+  // reset forms
   $("#pizza-form1").trigger("reset");
   $("#pizza-form2").trigger("reset");
-}
+  }
 
